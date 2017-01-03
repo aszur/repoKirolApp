3cda5ba9ce0cedd0b9a72daeee1df2b240cccb4a
Asier Unanue <aunanue001@gmail.com>
2017-01-03 15:43:27 +0100

    Prueba de permisos en runtime. No funciona. Pendiente de almacenar foto y mostrarla.

--- KirolApp/app/src/main/java/es/tta/kirolApp/RegisterActivity.java
+++ KirolApp/app/src/main/java/es/tta/kirolApp/RegisterActivity.java
@@ -1,11 +1,14 @@
 package es.tta.kirolApp;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -24,6 +27,7 @@ import es.tta.kirolApp.model.User;
 public class RegisterActivity extends AppCompatActivity {
     //NetworkRequests solicitud = new NetworkRequests();
     Uri pictureUri;
+    private static int MY_PERMISSIONS_REQUEST_CAMERA=0;
     public static final int PICTURE_REQUEST_CODE = 1;
     User usuario = new User();
     EditText Tnombre;
@@ -37,6 +41,35 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        int permissionCheck = ContextCompat.checkSelfPermission(RegisterActivity.this,
+                Manifest.permission.CAMERA);
+        System.out.println("Permiso: "+ permissionCheck);
+        //Request permissions
+        if (ContextCompat.checkSelfPermission(RegisterActivity.this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(RegisterActivity.this,
+                    Manifest.permission.CAMERA)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(RegisterActivity.this,
+                        new String[]{Manifest.permission.READ_CONTACTS},
+                        MY_PERMISSIONS_REQUEST_CAMERA);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
     }
 
     public void registrarUsuario(){
@@ -160,5 +193,30 @@ public class RegisterActivity extends AppCompatActivity {
                 sendFile(pictureUri);
                 //break;
         }*/
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 0: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 }
